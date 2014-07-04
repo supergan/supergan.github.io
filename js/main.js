762f71ad137a1c8f287d874b2e2355e413a19e8c
supergan <superiorchuck@gmail.com>
2014-07-03 21:10:31 -0400

    Modified

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     var template = $("#surveyControlEntryTemplate").html();
     var compileTemplate = _.template(template);
     $("#surveyControlTable").append(compileTemplate());
-    $('.input-group.date').datepicker();
+    $('.form-control.input-xs.datePicker').datepicker();
     $('#addSurvey').on(
     	'click', function(){
     		$("#surveyControlTable").append(compileTemplate());
@@ -11,23 +11,16 @@ $(document).ready(function(){
     
     $('#submitData').on(
         	'click', function(){
-        		var finalResult = false;
-        		var datasetNum = $('form').length;
-        		var count=0;
-        		var timer=1000;
+        		$('#waitingGIF').eq(0).attr("style","visibility:visible");
+        		var surveyData =[];
         		_.each($('form'),function(data){
-        			$(data).find('#waitingGIF').eq(0).attr("style","visibility:visible");
-        			//make ajax call; use timer to mimic ajax callback
-        			console.log($(data).serialize());
-        			setTimeout(function(){
-        				count=count+1;
-        				console.log(count);
-        				$(data).find('#waitingGIF').eq(0).attr("src","pics/Check-Free_Use.png");
-        				if(count==datasetNum){
-        					console.log("complete");
-        				}}, timer);
-        			timer=timer+2000;
+        			surveyData.push($( data ).serializeArray());
         		});
+    			//make ajax call; use timer to mimic ajax callback
+    			setTimeout(function(){
+    				$('#waitingGIF').eq(0).attr("src","pics/Check-Free_Use.png");
+    				}, 3000);
+        		console.log(surveyData);
         		
         	}
         );
